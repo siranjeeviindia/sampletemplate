f76d93eccfbbf77ed40e6f9d4f04e96e944dbf9d
siranjeevi <siranjeeviindia@gmail.com>
2018-03-20 00:24:10 +0530

    commit not

--- src/sampleDynamic/SampleClass.java
+++ src/sampleDynamic/SampleClass.java
@@ -1,7 +1,10 @@
 package sampleDynamic;
 
+import java.math.BigDecimal;
+
 public class SampleClass {
 	public int sampleInt;
 	public String sampleString;
+	public BigDecimal sampleBigDecimal;
 
 }
